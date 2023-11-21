26a9dcc0c24b8bf5782151dcf8bf11b0e7ebcb04
Richard <richard.eth@foxmail.com>
2023-11-22 00:27:36 +0800

    优化代码，提升mint速度

--- mint/mintEvm20.py
+++ mint/mintEvm20.py
@@ -6,29 +6,31 @@ import time
 # 将 nonce 和 gas_price 声明为全局变量
 global_nonce = None
 global_gas_price = None
+
 def interactWeb3(web3, nonce, gas_price, chainId, chainName, adress, data, private_key, gas_limit, multiple):
     print(f'公链：{chainName}   钱包地址：{adress}')
     # gas_price = web3.eth.gas_price
     print(f'当前gas价格:{gas_price / 1_000_000_000} gwei')
     currentGas = int(gas_price / 1_000_000_000)
-
     if gas_limit != '' and currentGas > int(gas_limit):
         return -1
     else:
+        fibal_gas_price = int(float(gas_price) * multiple)
         tx = {
-            'nonce': web3.eth.get_transaction_count(adress),
+            'nonce': nonce,
             'chainId': chainId,
             'to': adress,
             'from': adress,
             'data': data,  # mint 16进制数据
-            'gasPrice': int(gas_price * multiple),
-            'value': Web3.to_wei(0, 'ether')
+            'gasPrice': fibal_gas_price,
+            'value': Web3.to_wei(0, 'ether'),
+            'gas':22200
         }
         # nonce = web3.eth.get_transaction_count(adress)
-        tx['nonce'] = nonce
+        # tx['nonce'] = nonce
         # gas = web3.eth.estimate_gas(tx)
         # tx['gas'] = gas
-        tx['gas'] = '22200'
+        # tx['gas'] = 22200
         print(f"交易消耗代币数量：{(tx['gasPrice'] / 1_000_000_000 * tx['gas']) / 1_000_000_000}")
         signed_tx = web3.eth.account.sign_transaction(tx, private_key)
         tx_hash = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
@@ -58,13 +60,13 @@ def mint(pwd):
                     global_nonce = web3.eth.get_transaction_count(adress)
                     global_gas_price = web3.eth.gas_price
                 else:
+                    print('global_nonce:',global_nonce)
                     receipt = interactWeb3(web3, global_nonce, global_gas_price, chainId, chainName, adress, data, private_key,
                                            gas_limit,
                                            mulriple)
                     if receipt == -1:
-                        # time.sleep(3)
                         print(F'~~~等待gas下降到{gas_limit}才铸造~~~')
-                    elif receipt.status == 1:
+                    elif receipt == 1:
                         success += 1
                         print("~~~Mint Success~~~")
                     else:
@@ -78,6 +80,7 @@ def mint(pwd):
                 failed += 1
             n += 1
             times += 1
+            global_nonce += 1
             print(F'{success} Success,{failed} Failed!\n\n')
             time.sleep(delay)
     else:
@@ -92,9 +95,8 @@ def mint(pwd):
                                            gas_limit,
                                            mulriple)
                     if receipt == -1:
-                        # time.sleep(3)
                         print(F'~~~等待gas下降到{gas_limit}才铸造~~~')
-                    elif receipt.status == 1:
+                    elif receipt == 1:
                         success += 1
                         print("~~~Mint Success~~~")
                     else:
@@ -107,6 +109,7 @@ def mint(pwd):
                     print(f'Mint ERROR:{e}')
                 failed += 1
             times += 1
+            global_nonce += 1
             print(F'{success} Success,{failed} Failed!\n\n')
             time.sleep(delay)
         print(f'成功：{success}\n失败:{failed}')
